9ff26daf28d705511795ac28ca5e4404583f6cb0
gepo01 <gepo01@3e917089-2bf4-0310-95d7-c3653a530a58>
2005-04-11 18:17:24 +0000

    sdfsdf

---
+++ trunk/siri/test/src/Nisse.java
@@ -0,0 +1,10 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gepo01
+ * Date: 2005-apr-11
+ * Time: 20:07:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class Nisse {
+}
