9d4c0e6279d537bbfdf9b947ff9498d51f57c4ed
gepo01 <gepo01@3e917089-2bf4-0310-95d7-c3653a530a58>
2005-04-11 18:01:49 +0000

    testing

---
+++ trunk/siri/test/src/Test.java
@@ -0,0 +1,10 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gepo01
+ * Date: 2005-apr-10
+ * Time: 12:09:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
