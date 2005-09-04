c90037df7b2c8f2b91a1ec80e85fc3806098cf91
gepo01 <gepo01@3e917089-2bf4-0310-95d7-c3653a530a58>
2005-09-04 14:54:43 +0000

    deleted

--- trunk/modules/fatview/src/schema/jaxb.irisclientsettings.bat
+++
@@ -1,5 +0,0 @@
-set JAVA_HOME=C:\java\JBuilder9\jdk1.4
-
-C:\java\jwsdp-1.3\jaxb\bin\xjc.bat irisclient.xsd -d ../java -p org.iris.client.settings.xml
-
-pause
--- trunk/modules/xml-binding/src/schema/jaxb.core-server-config.bat
+++
@@ -1,8 +0,0 @@
-REM Use maven build file to do this!!!!!
-
-
-set JAVA_HOME=C:\java\JBuilder9\jdk1.4
-
-C:\java\jwsdp-1.3\jaxb\bin\xjc.bat iris.xsd -d ../java -p org.iris.xml.server.settings
-
-pause
