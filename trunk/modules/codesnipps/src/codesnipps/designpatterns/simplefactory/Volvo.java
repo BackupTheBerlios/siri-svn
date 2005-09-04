7d05013f3465903a0367bf34756c63e8a0f3e537
gepo01 <gepo01@3e917089-2bf4-0310-95d7-c3653a530a58>
2005-09-04 16:31:18 +0000

    e

---
+++ trunk/modules/codesnipps/src/codesnipps/designpatterns/simplefactory/Volvo.java
@@ -0,0 +1,12 @@
+package codesnipps.designpatterns.simplefactory;
+
+import codesnipps.designpatterns.factory.Car;
+
+public class Volvo extends Car{
+
+	Car createCar(String type) {
+		1
+		return null;
+	} 
+
+}
