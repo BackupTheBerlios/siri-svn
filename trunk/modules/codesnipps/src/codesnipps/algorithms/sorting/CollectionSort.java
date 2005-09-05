0377471630bd98d6d1d808a802967c1ce40983dd
gepo01 <gepo01@3e917089-2bf4-0310-95d7-c3653a530a58>
2005-09-05 18:59:21 +0000

    .

--- trunk/modules/codesnipps/src/codesnipps/algorithms/sorting/CollectionSort.java
+++ trunk/modules/codesnipps/src/codesnipps/algorithms/sorting/CollectionSort.java
@@ -8,10 +8,14 @@ import java.util.List;
  * Example taken from http://java.sun.com/docs/books/tutorial/collections/algorithms/index.html
  * 
  * # Fast: It is guaranteed to run in n log(n) time and runs substantially faster 
- * on nearly sorted lists. Empirical tests showed it to be as fast as a highly optimized 
- * quicksort. Quicksort is generally regarded to be faster than merge sort but isn't 
- * stable and doesn't guarantee n log(n) performance.
- * # Stable: It doesn't reorder equal elements. This is important if you sort the same list repeatedly on different attributes. If a user of a mail program sorts the in-box by mailing date and then sorts it by sender, the user naturally expects that the now-contiguous list of messages from a given sender will (still) be sorted by mailing date. This is guaranteed only if the second sort was stable.
+ * on nearly sorted lists. Empirical tests showed it to be as fast as a highly 
+ * optimized quicksort. Quicksort is generally regarded to be faster than merge 
+ * sort but isn't stable and doesn't guarantee n log(n) performance.
+ * # Stable: It doesn't reorder equal elements. This is important if you sort the 
+ * same list repeatedly on different attributes. If a user of a mail program sorts 
+ * the in-box by mailing date and then sorts it by sender, the user naturally 
+ * expects that the now-contiguous list of messages from a given sender will (still) 
+ * be sorted by mailing date. This is guaranteed only if the second sort was stable.
 
  * @author gepo
  *
