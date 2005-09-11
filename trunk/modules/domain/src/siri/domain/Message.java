bb28cec2ab2b2a1a4b2a75a9e19b00d7a35d7cb5
gepo01 <gepo01@3e917089-2bf4-0310-95d7-c3653a530a58>
2005-09-11 10:50:27 +0000

    .

--- trunk/modules/domain/src/siri/domain/Message.java
+++ trunk/modules/domain/src/siri/domain/Message.java
@@ -1,12 +1,15 @@
 package siri.domain;
 
+
 /**
  * Created by IntelliJ IDEA.
  * User: gepo
  * Date: 2005-sep-11
  * Time: 12:33:33
  * To change this template use File | Settings | File Templates.
+ * Date
  */
-public class Message   
+public class Message implements Serial
 {
+    
 }
