144a5921d8138dfea5e55d67ddcba53e03db9db8
gepo01 <gepo01@3e917089-2bf4-0310-95d7-c3653a530a58>
2005-11-10 12:19:29 +0000

    users ssasasa

---
+++ trunk/modules/domain/src/test/java/org/siri/dao/test/MessageDAOHibernateTest.java
@@ -0,0 +1,15 @@
+package org.siri.dao.hibernate.test;
+/**
+ * Created by IntelliJ IDEA.
+ * User: geopol
+ * Date: 2005-nov-10
+ * Time: 13:20:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+import junit.framework.*;
+import org.siri.dao.hibernate.MessageDAOHibernate;
+
+public class MessageDAOHibernateTest extends TestCase {
+    MessageDAOHibernate messageDAOHibernate;
+}
